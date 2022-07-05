ffa7f9c2feb9db61c3bf735cae759568a9031601
Prybrt06 <prybrt06@gmail.com>
2022-07-05 13:27:15 +0530

    Initial Commit

---
+++ Hacker News/Hacker_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hacker_NewsApp.swift
+//  Hacker News
+//
+//  Created by Priyabrat  Duarah on 05/07/22.
+//
+
+import SwiftUI
+
+@main
+struct Hacker_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
